1b87f55ba2b3dce61531d50c601112e92f9462e2
Bappa Banerjee <bappa3727@gmail.com>
2023-04-14 13:07:22 +0530

    navbar long-loading fixed

--- src/components/TranzoNavbar.jsx
+++ src/components/TranzoNavbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 import './css/tranzo_nav.css';
 // import Lin
 
@@ -9,13 +10,13 @@ const TranzoNavbar = () => {
     return (
         <Navbar className='nav_bar' expand="lg">
             <Container>
-                <Navbar.Brand className='nav_bar_title' href="/">Tranzo</Navbar.Brand>
+                <Navbar.Brand className='nav_bar_title' as={Link} to="/">Tranzo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto ">
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
-                        <Nav.Link href="/transactions">Transactions</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/transactions">Transactions</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
